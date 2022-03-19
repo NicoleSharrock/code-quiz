5dca6577a475d99e4c5001f7037b058597d170e3
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-03-19 16:27:53 -0400

    added funtion to my sart button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,100 @@
 const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
 const questionContainerElement = document.getElementById("question-container")
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame)
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
     console.log("started")
     startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove("hide")
     setNextQuestion()
 }
 
-// var questions = [
-//     {
-//         prompt: "___ is the process of finding errors and fixing them within a program.\n(a) Compiling\n\(b) Executing\n(c) Debugging\n(d) Scanning",
-//         answer: "c"
-//     },
-//     {
-//         prompt: "A loop that never ends is referred to as a(n) ___.\n(a) While loop\n\(b) Infinite loop\n(c) Recursive loop\n(d) for loop",
-//         answer: "b"
-//     },]
-// var score = 0;
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
-// for (var i = 0; i < questions.length; i++) {
-//     var response = window.prompt(questions[i].prompt);
-//     if (response == questions[i].answer) {
-//         score++;
-//         alert("Correct!");
-//     } else {
-//         alert("Wrong!");
-//     }
-// }
-// alert("you got " + score + "/" + questions.length)
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answer.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "Restart"
+        startButton.classList.remove("hide")
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}
+
+
+const questions = [
+    {
+        question: '_______ is the process of finding errors and fixing them within a program.',
+        answers: [
+            { text: 'Compiling', correct: false },
+            { text: 'Executing', correct: false },
+            { text: 'Debugging', correct: true },
+            { text: 'Scanning', correct: false }
+        ]
+    },
+    {
+        question: 'A loop that never ends is referred to as a(n) _____?',
+        answers: [
+            { text: 'While loop', correct: false },
+            { text: 'Infinite loop', correct: true },
+            { text: 'Recursive loop', correct: false },
+            { text: 'for loop', correct: false }
+        ]
+    }
+
+]
